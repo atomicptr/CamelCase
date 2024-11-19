ab07b2085f60c1d5c5e062bc090c67b60f223837
Christopher Kaster <me@atomicptr.de>
2024-11-19 17:46:39 +0100

    add more expect_* functions for the value types

--- lib/Values.ml
+++ lib/Values.ml
@@ -19,9 +19,38 @@ module StringComparable : ComparableValue with type t = string = struct
   let to_string s = s
 end
 
+let check_result check error_msg = if check then TestResult.Success else TestResult.Failure error_msg
+
 module MakeComparableValue (T : ComparableValue) = struct
   (** Asserts that two values for the given type are equal *)
   let expect_equals expected input =
-    if T.compare expected input = 0 then TestResult.Success
-    else TestResult.Failure ("expected: " ^ T.to_string input ^ " to be: " ^ T.to_string expected)
+    check_result (T.compare input expected = 0) ("expected: " ^ T.to_string input ^ " to be: " ^ T.to_string expected)
+
+  (** Asserts that two values aren't equal *)
+  let expect_not_equals expected input =
+    check_result (T.compare input expected != 0) ("expected: " ^ T.to_string input ^ " to be: " ^ T.to_string expected)
+
+  (** Asserts that expected input to be smaller than expected *)
+  let expect_smaller expected input =
+    check_result
+      (T.compare input expected < 0)
+      ("expected: " ^ T.to_string input ^ " to be smaller than " ^ T.to_string expected)
+
+  (** Asserts that expected input to be smaller than expected or the same *)
+  let expect_smaller_or_equal expected input =
+    check_result
+      (T.compare input expected <= 0)
+      ("expected: " ^ T.to_string input ^ " to be smaller or equal than " ^ T.to_string expected)
+
+  (** Asserts that expected input to be bigger than expected *)
+  let expect_bigger expected input =
+    check_result
+      (T.compare input expected > 0)
+      ("expected: " ^ T.to_string input ^ " to be bigger than " ^ T.to_string expected)
+
+  (** Asserts that expected input to be bigger than expected or the same *)
+  let expect_bigger_or_equal expected input =
+    check_result
+      (T.compare input expected >= 0)
+      ("expected: " ^ T.to_string input ^ " to be bigger or equal than " ^ T.to_string expected)
 end
--- test/Test.ml
+++ test/Test.ml
@@ -12,9 +12,14 @@ let () =
       test "simple: chaining expects" (fun () -> expect_true true >> expect_true true >> expect_false false);
       test "expect_true returns failure with false" (fun () -> expect_failure @@ expect_true false);
       test "expect: 2 + 2 = 4" (fun () -> IntValue.expect_equals 4 (2 + 2));
+      test "expect: 'Hello' != 'World'" (fun () -> StringValue.expect_not_equals "Hello" "World");
       test "expect failure: 2 + 2 = 5" (fun () -> expect_failure @@ IntValue.expect_equals 5 (2 + 2));
       test "expect Option is some" (fun () -> expect_some (Some 1337));
       test "expect Option is none" (fun () -> expect_none None);
       test "expect Result is ok" (fun () -> expect_ok (Ok 42));
       test "expect Result is error" (fun () -> expect_error (Error "Nope"));
+      test "expect: 2 < 4" (fun () -> IntValue.expect_smaller 4 2);
+      test "expect: 4 <= 4" (fun () -> IntValue.expect_smaller_or_equal 4 4);
+      test "expect: 5 > 1" (fun () -> IntValue.expect_bigger 1 5);
+      test "expect: 5 >= 5" (fun () -> IntValue.expect_bigger_or_equal 5 5);
     ]
